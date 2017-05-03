03dee042aa7aff08e5ed2766cf43a1049aea6e83
DONGSHIH TENG <tou9702@gmail.com>
2017-05-03 18:42:57 +0900

    Add more row

--- display/toumenu.py
+++ display/toumenu.py
@@ -3,8 +3,8 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 blank     = [  0,   0,   0]
-tm_yellow = [255, 207, 132]
-tm_orange = [255, 161, 104]
+tm_yellow = [255, 220, 140]
+tm_orange = [254, 150, 100]
 tm_red    = [255,  70,  18]
 tm_purple = [209,  99, 206]
 
@@ -17,14 +17,14 @@ p = tm_purple
 
 
 image = [
-    b,b,b,b,b,b,b,b,
     b,b,o,b,b,r,b,b,
     y,b,o,o,r,r,b,p,
     y,y,o,o,r,r,p,p,
     y,y,o,o,r,r,p,p,
+    y,y,o,o,r,r,p,p,
+    y,y,o,o,r,r,p,p,
     y,b,o,o,r,r,b,p,
-    b,b,o,b,b,r,b,b,
-    b,b,b,b,b,b,b,b
+    b,b,o,b,b,r,b,b
 ]
 
 sense.set_pixels(image)
