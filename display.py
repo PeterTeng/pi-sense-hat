55651f48a223e22d6a643918398a5291619c8507
DONGSHIH TENG <tou9702@gmail.com>
2017-05-03 18:11:38 +0900

    Add more arugument

--- display.py
+++ display.py
@@ -2,4 +2,6 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-sense.show_message("Hello! I am Peter")
+sense.show_message("Hello World", scroll_speed=0.05, text_colour=[255,255,0], back_colour=[0,0,255])
+
+sense.clear()
