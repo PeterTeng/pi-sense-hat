9c2dd74e0e0e7c63812b9ad2fccfb2cba2645298
DONGSHIH TENG <tou9702@gmail.com>
2017-05-03 19:08:20 +0900

    Refactor

--- sensors/environment.py
+++ sensors/environment.py
@@ -1,6 +1,9 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 
+def show_slow_text(text):
+    sense.show_message(text, scroll_speed=0.05, text_colour=[170, 170, 170])
+
 while True:
     temperature = sense.get_temperature()
     pressure    = sense.get_pressure()
@@ -10,6 +13,11 @@ while True:
     pressure    = round(pressure, 1)
     humidity    = round(humidity, 1)
 
-    msg = "Temperature = %s, Pressure=%s, Humidity=%s" % (temperature, pressure, humidity)
+    temperature_text = "%s 'C"　% (temperature)
+    sense.show_slow_text(temperature_text)
 
-    sense.show_message(msg, scroll_speed=0.05)
+    pressure_text = "%s hPa"　% (pressure)
+    sense.show_slow_text(pressure_text)
+
+    humidity_text = "%s %"　% (humidity)
+    sense.show_slow_text(humidity_text)
