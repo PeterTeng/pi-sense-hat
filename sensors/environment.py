b31fbaf25ff5667d56956e70cdcbb482065ad2ee
DONGSHIH TENG <tou9702@gmail.com>
2017-05-03 19:11:41 +0900

    Try

--- sensors/environment.py
+++ sensors/environment.py
@@ -2,7 +2,7 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 def show_slow_text(text):
-    sense.show_message(text, scroll_speed=0.05, text_colour=[170, 170, 170])
+    sense.show_message(text, scroll_speed=0.05, text_colour=[120, 120, 120])
 
 while True:
     temperature = sense.get_temperature()
@@ -13,6 +13,12 @@ while True:
     pressure    = round(pressure, 1)
     humidity    = round(humidity, 1)
 
-    msg = "Temperature = %s, Pressure=%s, Humidity=%s" % (temperature, pressure, humidity)
+    temperature_text = "%s C" % (temperature)
+    humidity_text = "%s %" % (humidity)
+    pressure_text = "%s hpa" % (pressure)
 
-    show_slow_text(msg)
+    show_slow_text(temperature_text)
+
+    show_slow_text(humidity_text)
+
+    show_slow_text(pressure_text)
